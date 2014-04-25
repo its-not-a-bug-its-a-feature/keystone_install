fba4479e8f1c50c9bb5c0011f013a46bac721a27
David Cole <david.cole@sohonet.com>
2014-04-25 12:21:54 +0100

    David - Correcting output echo'ed at end of run

--- install_keystone.sh
+++ install_keystone.sh
@@ -187,22 +187,17 @@ sleep 5
 
 echo "===========Keystone Middleware setting for this deployment============="
 
-echo "[ Keystone Auth ]"
-echo "operator_roles : admin, swiftoperator, _member_"
-echo "reseller_prefix : KEY_"
+echo "[ Keystone Auth - filter:keystoneauth ]"
+echo "operator_roles : admin, swiftoperator"
+echo "reseller_prefix : AUTH_"
 echo "reseller_admin_role : ResellerAdmin"
 echo
-echo "[Keystone Auth Token Support]"
-echo "auth_admin_prefix : (leave blank)"
+echo "[Keystone Auth Token - filter:authtoken]"
 echo "auth_host : \$IP_OF_KEYSTONE_HOST"
 echo "auth_port : 35357"
 echo "auth_protocol : http"
-echo "auth_uri : http://\$KEYSTONE_IP:5000/"
-echo "admin_user : swift"
-echo "admin_password : password"
-echo "admin_tenant_name : service"
-echo "signing_dir : /var/cache/swift"
-echo "include_service_catalog : False"
+echo "admin_token = \$SERVICE_TOKEN"
+echo ""
 
 echo "========== DB information =========="
 echo "user : root"
